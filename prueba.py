47b04034f7630404dd93757a3705a44fc3e77113
advarros-del <advarros@gmail.com>
2026-05-18 22:48:02 +0200

    Suficiente por hoy

--- prueba.py
+++ prueba.py
@@ -29,47 +29,42 @@ def make_the_maze(total_height_size: int, total_width_size: int) -> list[list[in
     temp_cord: tuple
     flag: bool = True
     while len(visited) != (total_height_size * total_width_size):
-        temp_cord = current_cord + random_direc
-        if random_direc == up:
-            if current_cord[0] + random_direc[0] > 0:
-                for h in visited:
-                    if temp_cord == h:
-                        flag = False
-                if flag:
-                    current_cord = temp_cord
-                    visited.append(current_cord)
-                    num_matrix[current_cord[0]][current_cord[1]] += 1      #vacio!
-                    num_matrix[current_cord[0]+ 1][current_cord[1]] += 2   #rompo pared de abajo y    juntanterior con este
-        elif random_direc == down:
-            if current_cord[0] + random_direc[0] < total_height_size:
-                for h in visited:
-                    if temp_cord == h:
-                        flag = False
-                if flag:
-                    current_cord = temp_cord
-                    visited.append(current_cord)
-                    num_matrix[current_cord[0]][current_cord[1]] += 2       #vacio
-                    num_matrix[current_cord[0 - 1]][current_cord[1]] += 1    #rompo pared de arriba y junto anterior con este
-        elif random_direc == right:
-            if current_cord[1] + random_direc[1] < total_width_size:
-                for h in visited:
-                    if temp_cord == h:
-                        flag = False
-                if flag:
-                    current_cord = temp_cord
-                    visited.append(current_cord)
-                    num_matrix[current_cord[0]][current_cord[1]] += 4        #vacio
-                    num_matrix[current_cord[0]][current_cord[1] - 1] += 8    #rompo pared de izquierda y junto con lo anterior
-        elif random_direc == left:
-            if random_direc[1] + random_direc[1] > 2:
-                for h in visited:
-                    if temp_cord == h:
-                        flag = False
-                if flag:
-                    current_cord = temp_cord
-                    visited.append(current_cord)
-                    num_matrix[current_cord[0]][current_cord[1]] += 8        #vacio 
-                    num_matrix[current_cord[0]][current_cord[1]+ 1] += 4     #rompo pared de derecha y junto con lo anterior
+        temp_cord = (current_cord[0] + random_direc[0], current_cord[1] + random_direc[1])
+        neighbors: list[tuple] = [
+            (temp_cord[0] + up[0], temp_cord[1] + up[1]),
+            (temp_cord[0] + down[0], temp_cord[1] + down[1]),
+            (temp_cord[0] + right[0], temp_cord[1] + right[1]),
+            (temp_cord[0] + left[0], temp_cord[1] + left[1])
+        ]
+        if all(n in visited for n in neighbors):
+                current_cord = trash.pop()
+        flag = True
+        if temp_cord in visited:
+            flag = False
+        if not (0 <= temp_cord[0] < total_height_size and 0 <= temp_cord[1] < total_width_size):
+            flag = False
+        if flag:
+            current_cord = temp_cord
+            if random_direc == up:
+                visited.append(current_cord)
+                trash.append(current_cord)
+                num_matrix[current_cord[0]][current_cord[1]] += 1      #vacio!
+                num_matrix[current_cord[0]+ 1][current_cord[1]] += 2   #rompo pared de abajo y    juntanterior con este
+            elif random_direc == down:
+                visited.append(current_cord)
+                trash.append(current_cord)
+                num_matrix[current_cord[0]][current_cord[1]] += 2       #vacio
+                num_matrix[current_cord[0] - 1][current_cord[1]] += 1    #rompo pared de arriba y junto anterior con este
+            elif random_direc == right:
+                visited.append(current_cord)
+                trash.append(current_cord)
+                num_matrix[current_cord[0]][current_cord[1]] += 4        #vacio
+                num_matrix[current_cord[0]][current_cord[1] - 1] += 8    #rompo pared de izquierda y junto con lo anterior
+            elif random_direc == left:
+                visited.append(current_cord)
+                trash.append(current_cord)
+                num_matrix[current_cord[0]][current_cord[1]] += 8        #vacio 
+                num_matrix[current_cord[0]][current_cord[1]+ 1] += 4     #rompo pared de derecha y junto con lo anterior
         random_direc = random.choice(direcctions)
     
     
@@ -79,5 +74,5 @@ def make_the_maze(total_height_size: int, total_width_size: int) -> list[list[in
     
     return num_matrix
 
-make_the_maze(15, 15)
+make_the_maze(5, 5)
 
